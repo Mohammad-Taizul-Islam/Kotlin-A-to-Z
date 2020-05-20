339be94d5f9ed5a8051d7678a6d01c4b20d8355d
Mohammad-Taizul-Islam <m.taizul1045@gmail.com>
2020-05-20 20:51:00 +0600

    Kotlin Data class basic feature

---
+++ DataClass/src/Main.kt
@@ -0,0 +1,6 @@
+fun main()
+{
+    val p1=Person("Satter",34)
+    println("Person's name : ${p1.name}")
+    println("Person's age : ${p1.age}")
+}
---
+++ DataClass/src/Person.kt
@@ -0,0 +1,2 @@
+//Doesn't need getter and setter
+data class Person(val name:String,var age:Int)
